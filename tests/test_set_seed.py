88872c8dc2e35486e491323abcc9962481faa433
himkt <himkt@cookpad.com>
2019-09-25 20:01:25 +0900

    Update test (dirty workaround)

--- tests/test_set_seed.py
+++ tests/test_set_seed.py
@@ -1,21 +1,29 @@
 from chainer_util import set_seed
 from chainer import testing
 
-import chainer
 import pytest
 
 
 def get_random_vector(device, seed):
+    import chainer
+    import numpy
+    import random
     set_seed(seed)
-    return chainer.get_device(device).xp.random.random(10)
+    x = chainer.get_device(device).xp.random.random(10)
+    del chainer
+    del numpy
+    del random
+    return x
 
 
 @pytest.mark.parametrize("device", [-1, 0])
 def test_set_seed(device: int):
+    import chainer
     if not chainer.cuda.available and device >= 0:
         pytest.skip("GPU is not available")
+    del chainer
 
     seed = 42
     x1 = get_random_vector(device, seed)
     x2 = get_random_vector(device, seed)
-    assert testing.assert_allclose(x1, x2)
+    assert sum(x1 - x2) == 0.0
